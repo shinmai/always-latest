3fce3a2f59d6147878874c06d32c40f8f6885140
Aapo Saaristo <aapo.saaristo@gmail.com>
2020-03-30 10:30:12 +0300

    Update inject.js

--- inject.js
+++ inject.js
@@ -4,12 +4,17 @@
     if (document.readyState === "complete" && document.getElementsByTagName("h2").length > 2) {
       clearInterval(getReadyToRun)
       if(window.location.host != "twitter.com" || window.location.pathname != "/home") return
-      let entry = document.querySelector("a[href='/settings/content_preferences']")
+      let entry = document.querySelector("[aria-label='Top Tweets on']")
       if(!entry) return
-      let link = entry.previousSibling
-      if(!link) return
-      if(link.innerText.split('\n')[1].toLowerCase().indexOf('see top tweets')) return
-      link.click()
+      entry.click()
+      setTimeout(() => {
+        let focus = document.querySelector("a[href='/settings/content_preferences']")
+        if(!entry) return
+        let link = focus.previousSibling
+        if(!link) return
+        if(link.innerText.split('\n')[1].toLowerCase().indexOf('see top tweets') != -1) return
+        link.click()
+      }, 100)
     }
   }, 10)
 })()
